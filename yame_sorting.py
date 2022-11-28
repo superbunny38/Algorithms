6f54d1457f190de3bd4c66b7f2a9f4ea90856e32
Chaeeun Ryu <48243487+superbunny38@users.noreply.github.com>
2022-11-28 17:51:27 +0900

    Add files via upload

---
+++ yame_sorting.py
@@ -0,0 +1,12 @@
+#야매 정렬
+def ex_oj_print(L):
+    length = len(L)
+    for idx, num in enumerate(L):
+        if idx == length -1:
+            print(f" {num}")
+        else:
+            print(f" {num}",end = "")
+
+N = int(input())
+num_list = [int(x) for x in input().split()]
+ex_oj_print(sorted(num_list))
