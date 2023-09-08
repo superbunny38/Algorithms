86eb727e8588c2ece31966754777eb9440af5b47
Chaeeun Ryu <48243487+superbunny38@users.noreply.github.com>
2023-09-08 16:55:23 +0900

    Update 1987.py -- 시간초과 안 나게 다시 수정.. 성공..

--- CHCoTe/1987.py
+++ CHCoTe/1987.py
@@ -4,29 +4,28 @@
 #그래프
 
 import sys
+sys.setrecursionlimit(10000)
+
 input = sys.stdin.readline
 
 R,C = map(int, input().split())
-graph = []
-for _ in range(R):
-    tmp = list(input())[:C]
-    graph.append(tmp)
+graph = [list(input()) for _ in range(R)]
 
-visited = set()
+visited_q = set([(0,0,graph[0][0])])
 
 max_cnt = 0
+dx,dy = [0,0,1,-1],[1,-1,0,0]
 
-def dfs(x,y,cnt):
+def bfs(visited_q):
     global max_cnt
-    if max_cnt < cnt:
-        max_cnt = cnt
-    dx,dy = [0,0,1,-1],[1,-1,0,0]
-    for move_x, move_y in zip(dx,dy):
-        if 0<=x+move_x<C and 0<=y+move_y<R and graph[y+move_y][x+move_x] not in visited:
-            visited.add(graph[y+move_y][x+move_x])
-            dfs(x+move_x,y+move_y,cnt+1)
-            visited.remove(graph[y+move_y][x+move_x])
-
-visited.add(graph[0][0])    
-dfs(0,0,1)   
+    while visited_q:
+        y,x,path = visited_q.pop()
+        if max_cnt < len(path):
+            max_cnt = len(path)
+        for move_x, move_y in zip(dx,dy):
+            if 0<=x+move_x<C and 0<=y+move_y<R and graph[y+move_y][x+move_x] not in path:
+                visited_q.add((y+move_y,x+move_x,path+graph[y+move_y][x+move_x]))
+        
+     
+bfs(visited_q)   
 print(max_cnt)
