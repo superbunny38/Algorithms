b90e7ef39c3b48f2d980f9d491ee28f8c0e2f593
Chaeeun Ryu <48243487+superbunny38@users.noreply.github.com>
2024-08-21 09:22:13 -0400

    Create 94.py -- Binary Tree Inorder Traversal

---
+++ LeetCode/94.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+def printInorder(root, storage):
+    if root:
+        printInorder(root.left, storage)
+        storage.append(root.val)
+        printInorder(root.right,storage)
+    return storage
+
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if root:
+            st = printInorder(root, [])
+            return st
+        else:
+            return []
