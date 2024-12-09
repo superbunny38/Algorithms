15ab1e48eed6c0eafd866b3c98434c17f823882f
Chaeeun Ryu <48243487+superbunny38@users.noreply.github.com>
2024-12-09 01:39:21 -0500

    Add files via upload

---
+++ 24Fall_AoA/dijkstra.py
@@ -0,0 +1,33 @@
+import heapq
+
+'''
+O(V + E log V): The time complexity of Dijkstra's algorithm with a min-priority queue
+'''
+
+def dijkstra(graph, start):
+    queue = [(0, start)]
+    distances = {node: float('inf') for node in graph}
+    distances[start] = 0
+
+    while queue:
+        current_distance, current_node = heapq.heappop(queue)
+
+        if current_distance > distances[current_node]:
+            continue
+
+        for neighbor, weight in graph[current_node].items():
+            distance = current_distance + weight
+
+            if distance < distances[neighbor]:
+                distances[neighbor] = distance
+                heapq.heappush(queue, (distance, neighbor))
+
+    return distances
+
+graph = {
+    'A': {'B': 1, 'C': 4},
+    'B': {'A': 1, 'C': 2, 'D': 5},
+    'C': {'A': 4, 'B': 2, 'D': 1},
+    'D': {'B': 5, 'C': 1}
+}
+print(dijkstra(graph, 'A'))
