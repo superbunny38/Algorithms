e0ea826c9c6a590e39ecbf0108bc6febdba5a53b
Chaeeun Ryu <48243487+superbunny38@users.noreply.github.com>
2025-11-25 21:26:18 -0500

    Add files via upload

--- C++ Study/Chapter1Mathematics/Chapter1Mathematics.cpp
+++ C++ Study/Chapter1Mathematics/Chapter1Mathematics.cpp
@@ -28,10 +28,11 @@ unsigned int gcd2(unsigned int a, unsigned int b){
     return a;
 }
 
-unsigned int lcm(){
-
+//q3
+unsigned int lcm(unsigned int a, unsigned int b){
+    int h = gcd(a,b);
+    return h ? (a*(b/h)):0;
 }
-
 
 
 void q2(unsigned int a, unsigned int b){
@@ -40,11 +41,31 @@ void q2(unsigned int a, unsigned int b){
     cout << "\nw/o recursion:"<<gcd2(a,b);
 }
 
+//q4
+bool is_prime(int const num){
+    if (num <= 3){
+        return num >1;
+    }
+
+    else if (num %2 == 0 || num % 3 == 0){
+        return false;
+    }
+    else{
+        for (int i = 5; i*i<=num; i += 6){
+            if (num%i == 0 || num% (i+2) == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
 int main(){
     int ans1;
     ans1 = q1();
     cout << "1. Answer:"<<ans1;
     cout << "\n\n2. Answer:";
     q2(10,5);
+    cout << "\n3. Answer:" << lcm(10,5);
     return 0;
 }
