76741334c8b47a6dc549c3ffa8a55a013b87a2ba
Chaeeun Ryu <48243487+superbunny38@users.noreply.github.com>
2025-12-11 23:52:26 -0500

    Add files via upload

---
+++ C++ Study/Practice1/Practice1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+//cd Practice1
+//g++ Practice1.cpp -o my_program
+//./my_program
+
+int main(){
+    const unsigned long long c1 = 4e10;
+
+    cout<<c1<<endl;
+
+
+    //median
+    double x,y,z;
+    cin >> x >> y >> z;
+
+    return 0;
+}
