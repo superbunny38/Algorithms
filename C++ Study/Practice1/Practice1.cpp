c216b677544409c04cb8f98fef951fde2e4a4180
Chaeeun Ryu <48243487+superbunny38@users.noreply.github.com>
2025-12-12 10:09:47 -0500

    Add files via upload

--- C++ Study/Practice1/Practice1.cpp
+++ C++ Study/Practice1/Practice1.cpp
@@ -5,15 +5,36 @@ using namespace std;
 //g++ Practice1.cpp -o my_program
 //./my_program
 
+double findMedian_Conditional(double a, double b, double c) {
+    // The median is the number that is NOT the minimum AND NOT the maximum.
+    // It's the one in the middle.
+
+    if ((b < a && a < c) || (c < a && a < b)) {
+        // 'a' is in the middle (e.g., b < a < c or c < a < b)
+        return a;
+    } else if ((a < b && b < c) || (c < b && b < a)) {
+        // 'b' is in the middle
+        return b;
+    } else {
+        // 'c' must be in the middle (or all are equal, which is also c)
+        return c;
+    }
+}
+
 int main(){
     const double c1 {4e10};
 
     cout<<c1<<endl;
 
-
     //median
+    cout << "Enter three numbers (x y z): ";
     double x,y,z;
     cin >> x >> y >> z;
+
+    double median = findMedian_Conditional(x,y,z);
+
+    cout << "The numbers are: " << x << ", " << y << ", " << z << endl;
+    cout << "The median is: " << median << endl;
 
     return 0;
 }
