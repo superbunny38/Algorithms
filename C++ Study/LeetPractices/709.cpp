bffeef46366459b26783431af2b22823d2b2fc17
Chaeeun Ryu <48243487+superbunny38@users.noreply.github.com>
2026-01-02 17:55:54 -0500

    709

---
+++ C++ Study/LeetPractices/709.cpp
@@ -0,0 +1,12 @@
+#include <cctype>
+#include <string>
+
+class Solution {
+public:
+    string toLowerCase(string s) {
+        for (int i = 0; i < s.length(); i++) {
+            s[i] = tolower(s[i]);
+        }
+        return s;
+    }
+};
