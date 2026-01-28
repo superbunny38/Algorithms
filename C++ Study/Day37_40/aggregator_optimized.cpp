b7f90f21bc5858849f9fa38419e58622c9831b03
Chaeeun Ryu <48243487+superbunny38@users.noreply.github.com>
2026-01-27 21:45:06 -0500

    Thread Pool

---
+++ C++ Study/Day37_40/aggregator_optimized.cpp
@@ -0,0 +1,129 @@
+#include <future>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <queue>
+#include <functional>
+#include <mutex>
+#include <condition_variable>
+
+using namespace std;
+
+class ThreadPool{
+    // component 1) A place to store the fixed number of running threads.
+    // component 2) A place to store the pending tasks.
+    // component 3) Synchronization tools to prevent the threads from fighting over the queue (race conditions)
+    public:
+        ThreadPool(size_t numThreads) : stop(false){// initializes a boolean member variable named stop to the value false when a new ThreadPool object is created.
+            for(size_t i = 0;i<numThreads;++i){
+                workers.emplace_back(
+                    [this]{
+                        while (true)
+                        {
+                            function<void()> task;
+                            {//lock scope
+                                unique_lock<mutex> lock(this->queueMutex);
+
+                                //wait until the stop flag is true or the queue is not empty
+                                this->condition.wait(lock,
+                                    [this]{ return this->stop || !this->tasks.empty(); });
+                                
+                                //If stop flag is true and there are no tasks, exit the thread
+                                if(this->stop && this->tasks.empty()){
+                                    return;
+                                }
+
+                                //Get the next task from the queue
+                                task = move(this->tasks.front());
+                                this->tasks.pop();
+                            }//lodk released here
+                        //execute the task outside the lock wo that the other threads can grab other tasks parallely
+                        task();
+                        }
+                       
+                    }
+                );
+            }
+        };
+        ~ThreadPool();
+
+        template<class F>
+        auto enqueue(F&& f) -> future<decltype(f())>;
+    
+    private:
+        vector<thread> workers;
+        queue<function<void()>> tasks;
+
+        mutex queueMutex;
+        condition_variable condition;
+        bool stop;
+
+};
+
+string ServiceA(){
+    this_thread::sleep_for(chrono::milliseconds(200));
+    cout << "Service A completed\n";
+    return "Data from Service A";
+}
+
+vector<int> ServiceB(){
+    this_thread::sleep_for(chrono::milliseconds(500));
+    cout << "Service B completed\n";
+    return {1, 2, 3, 4, 5};
+}
+
+double ServiceC(){
+    this_thread::sleep_for(chrono::milliseconds(3000));
+    cout << "Service C completed\n";
+    return 42.0;
+}
+
+int main(){
+    auto start = chrono::high_resolution_clock::now();
+    
+    //Define the tasks
+    packaged_task<string()> taskA(ServiceA);
+    packaged_task<vector<int>()> taskB(ServiceB);
+    packaged_task<double()> taskC(ServiceC);
+
+    //Get the futures before starting the threads
+    future<string> futureA = taskA.get_future();
+    future<vector<int>> futureB = taskB.get_future();
+    future<double> futureC = taskC.get_future();
+
+    thread(move(taskA)).detach();
+    thread(move(taskB)).detach();
+
+    thread threadC(move(taskC));
+    if (threadC.joinable()){
+        threadC.detach(); // Detach the thread to allow it to run independently
+    }
+
+    future_status statusC = futureC.wait_for(chrono::milliseconds(100));
+    if (statusC == future_status::timeout){
+        cout << "Service C is taking too long, proceeding without it.\n";
+        cout << "cur elapsed:"
+             << chrono::duration_cast<chrono::milliseconds>(
+                    chrono::high_resolution_clock::now() - start).count() 
+             << " ms\n";
+    }
+    else{
+        double resultC = futureC.get();
+        cout << "Service C result: " << resultC << "\n";
+    }
+    
+    string resA = futureA.get(); 
+    cout << "Got A: " << resA << endl;
+
+    vector<int> resB = futureB.get();
+    cout << "Got B (size): " << resB.size() << endl;
+
+    auto end = chrono::high_resolution_clock::now();
+    cout << "Total elapsed time: " 
+         << chrono::duration_cast<chrono::milliseconds>(end - start).count() 
+         << " ms\n";
+
+    return 0;
+}
